e8fd7625c788a9e971fe6229c6a134c8cdb8f717
Raul <raulbaumgarten@hotmail.com>
2025-05-09 11:01:34 -0300

    Inicializando estudo

--- src/PlantaCasa.java
+++ src/PlantaCasa.java
@@ -1,10 +1,23 @@
-public class PlantaCasa {
+public class PlantaCasa implements Construcao {
+    @Override
+    public void pintar(String cor) {
+
+    }
+
+    @Override
+    public int calcularCustoConstrucao(int metroQuadrado) {
+        return 0;
+    }
+
+
     int metragem = 300;
     int numeroDeQuartos = 3;
     int numeroDeBanheiros = 2;
     int numeroDeGaragens = 1;
     String cor = "Cinza";
     String material = "Alvenaria";
+
+
 
     public void construir() {
         System.out.println("A casa foi construída com sucesso!");
@@ -14,6 +27,7 @@ public class PlantaCasa {
         System.out.println("Número de garagens: " + numeroDeGaragens);
         System.out.println("Material construindo da casa: " + material);
     }
+
 
     public void pintar() {
         System.out.println("Pintando a casa de " + cor);
