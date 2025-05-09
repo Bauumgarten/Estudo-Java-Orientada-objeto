9fa9fd31812530b4afae8daff58d0884f533d11b
Raul <raulbaumgarten@hotmail.com>
2025-05-08 21:08:59 -0300

    Inicializando estudo

--- src/PlantaCasa.java
+++ src/PlantaCasa.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class PlantaCasa {
+    int metragem = 300;
+    int numeroDeQuartos = 3;
+    int numeroDeBanheiros = 2;
+    int numeroDeGaragens = 1;
+    String cor = "Cinza";
+    String material = "Alvenaria";
+
+    public void construir() {
+        System.out.println("A casa foi construída com sucesso!");
+        System.out.println("Metragem: " + metragem + " m²");
+        System.out.println("Número de quartos: " + numeroDeQuartos);
+        System.out.println("Número de banheiros: " + numeroDeBanheiros + ", podendo ter um banheiro social");
+        System.out.println("Número de garagens: " + numeroDeGaragens);
+        System.out.println("Material construindo da casa: " + material);
+    }
+
+    public void pintar() {
+        System.out.println("Pintando a casa de " + cor);
+    }
 }
--- src/casa.java
+++ src/casa.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class casa {
+    public static void main(String[] args) {
+        PlantaCasa casa = new PlantaCasa();
+
+        //Atribuindo valores
+        casa.metragem = 300;
+        casa.numeroDeQuartos = 3;
+        casa.numeroDeBanheiros = 2;
+        casa.numeroDeGaragens = 1;
+        casa.cor = "Cinza";
+        casa.material = "Alvenaria";
+
+        //Construindo a casa
+        casa.construir();
+
+        //Pintando a casa
+        casa.pintar();
+    }
 }
